82542e0621f3bc1c3d98fc0486357890bb325e62
whitej68 <114672020+whitej68@users.noreply.github.com>
2023-08-25 13:17:01 -0400

    Add lines

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fpdf import FPDF
 import pandas as pd
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
 
 df = pd.read_csv("topics.csv")
 
@@ -11,14 +12,34 @@ for index, row in df.iterrows():
     pdf.add_page()
 
     # ln - break line
+    # Sets the header
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0,h=12, txt=row["Topic"], align="L",
              ln=1)
-    pdf.line(10, 21, 200, 21)
+
+    # Adds lines to header pages
+    for y in range(20, 298, 10):
+        pdf.line(10, y, 200, y)
+
+    # Set the footer
+    pdf.ln(265)
+    pdf.set_font(family="Times", style="I", size=8)
+    pdf.set_text_color(180,180,180)
+    pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
 
     for i in range(int(row["Pages"]) - 1):
         pdf.add_page()
         pdf.set_font(family="Times", style="B", size=24)
+
+        # Set the footer
+        pdf.ln(277)
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
+        # Adds lines to blank pages
+        for y in range(20, 298, 10):
+            pdf.line(10, y, 200, y)
 
 pdf.output("output.pdf")
